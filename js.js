1dc0c00a5a270b4ee4cd1df9adef53b258e020db
aniaje <102797232+aniaje@users.noreply.github.com>
2022-12-18 10:20:54 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,57 @@
+function toDateTime(date) {
+  let str = "";
+  let month, day, hour, min;
+
+  month = date.getUTCMonth() + 1;
+  month = month < 10 ? "0" + month : month;
+  day = date.getUTCDate();
+  day = day < 10 ? "0" + day : day;
+  hour = date.getUTCHours();
+  hour = hour < 10 ? "0" + hour : hour;
+  min = date.getUTCMinutes();
+  min = min < 10 ? "0" + min : min;
+
+  str += day + "/" + month;
+  str += " " + hour + ":" + min;
+  return str;
+}
+const datePicker = document.querySelector("#pizza-time");
+const warning = document.getElementsByClassName("warning");
+const img = document.querySelector("img");
+document.getElementById("form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  img.classList.add("hidden");
+  const pizzaTime = document.getElementById("pizza-time").value;
+  // const timeDisplay = document.querySelector("#display-time");
+  const displayStep4 = document.querySelector("#display-step4");
+  const displayStep3 = document.querySelector("#display-step3");
+  const displayStep2 = document.querySelector("#display-step2");
+  const displayStep1 = document.querySelector("#display-step1");
+
+  let date = new Date(pizzaTime);
+
+  let step1 = new Date(new Date(pizzaTime).getTime() - 25 * 60 * 60 * 1000);
+  let step2 = new Date(new Date(pizzaTime).getTime() - 3 * 30 * 60 * 1000);
+  let step3 = new Date(new Date(pizzaTime).getTime() - 2 * 30 * 60 * 1000);
+  let step4 = new Date(new Date(pizzaTime).getTime() - 1 * 60 * 60 * 1000);
+
+  displayStep1.innerHTML = `${toDateTime(
+    step1
+  )} <span class="">prepare the polish& put in the firdge </span>`;
+
+  displayStep2.innerHTML = `${toDateTime(
+    step2
+  )} <span> the the polish put of the fridge and wait  </span>`;
+
+  displayStep3.innerHTML = `${toDateTime(
+    step3
+  )} <span>pmake the dought and let it rest</span>`;
+
+  displayStep4.innerHTML = `${toDateTime(step4)} let the formed balls rest`;
+});
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+  button.classList.toggle("darkBtn");
+});
