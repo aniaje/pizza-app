8e2c29d29a81648ded29855a4848e8c2db429170
Ana <jezowskanna@gmail.com>
2022-12-26 23:54:39 +0100

    section height adjusted

--- js.js
+++ js.js
@@ -26,31 +26,33 @@ document.getElementById("form").addEventListener("submit", function (e) {
   img.classList.add("hidden");
   const pizzaTime = document.getElementById("pizza-time").value;
   // const timeDisplay = document.querySelector("#display-time");
-  const displayStep4 = document.querySelector("#display-step4");
-  const displayStep3 = document.querySelector("#display-step3");
-  const displayStep2 = document.querySelector("#display-step2");
-  const displayStep1 = document.querySelector("#display-step1");
+  const displaySteps = document.querySelector("#display-steps");
 
   let date = new Date(pizzaTime);
 
-  let step1 = new Date(date.getTime() - 25 * 60 * 60 * 1000);
-  let step2 = new Date(date.getTime() - 3 * 30 * 60 * 1000);
-  let step3 = new Date(date.getTime() - 2 * 30 * 60 * 1000);
-  let step4 = new Date(date.getTime() - 1 * 60 * 60 * 1000);
+  let step1 = new Date(date.getTime() - 24 * 60 * 60 * 1000);
+  let step2 = new Date(date.getTime() - 3 * 60 * 60 * 1000);
+  let step3 = new Date(date.getTime() - 2 * 60 * 60 * 1000);
+  let step4 = new Date(date.getTime() - 0 * 60 * 60 * 1000);
 
-  displayStep1.innerHTML = `${toDateTime(
+  //create element
+
+  displaySteps.innerHTML = `<span class="time">${toDateTime(
     step1
-  )} <span class="">prepare the polish& put in the firdge </span>`;
-
-  displayStep2.innerHTML = `${toDateTime(
+  )}</span><p class="time-desc"> start preparing the polish&and place it in the fridge</p>
+  <span class="time">${toDateTime(
     step2
-  )} <span> polish put in room temp </span>`;
-
-  displayStep3.innerHTML = `${toDateTime(
+  )} </span><p class="time-desc">remove poolish from the fridge and let it rest in room temp</p>
+  <span class="time">${toDateTime(
     step3
-  )} <span>pmake the dought and let it rest</span>`;
-
-  displayStep4.innerHTML = `${toDateTime(step4)} let the formed balls rest`;
+  )} </span><p class="time-desc">begin with making the dough</p>
+  <span class="time">${toDateTime(
+    step4
+  )}</span><p class="time-desc">let the formed ready balls rest for 1h :) and your're ready to go!
+  </p>
+  <span class="note">*poolish fridge time: 20hrs</span>
+  <span class="note">*change the date to try different time</span>
+  `;
 });
 
 button.addEventListener("click", () => {
